b7a8547d359dc15120b037c22263c3356b3f4bfb
María Carolina Bklasco <mcarolinablasco25@gmail.com>
2024-12-05 15:35:19 -0300

    main editado

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 
 # Cargar el dataset procesado
 movies = pd.read_csv('C:/Users/Admin/Desktop/PROYECTO INDIVIDUAL 1º/Movies/processed_movies_dataset.csv', low_memory=False)
+credits= pd.read_csv('C:\Users\Admin\Desktop\PROYECTO INDIVIDUAL 1º\Movies\processed_credits_dataset.csv')
 
 @app.get("/")
 def read_root():
